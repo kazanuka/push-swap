eba930ef6bdf1b68a32012f141b86b358c6e1193
Furkan Kuyumcu <fkuyumcu@k1m30s11.42istanbul.com.tr>
2024-12-26 19:47:35 +0300

    swp

--- etc/sort.c
+++ etc/sort.c
@@ -6,7 +6,7 @@
 /*   By: fkuyumcu <fkuyumcu@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/12/26 17:14:44 by fkuyumcu          #+#    #+#             */
-/*   Updated: 2024/12/26 17:16:15 by fkuyumcu         ###   ########.fr       */
+/*   Updated: 2024/12/26 19:42:16 by fkuyumcu         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,7 +14,19 @@
 
 void	sort(t_stack **a, t_stack **b)
 {
-    
+    if (ft_lstsize(*a) <= 5)
+		simple_sort(a, b);
+	else
+		complex_sort(a,b);
+}
+
+static void simple_sort(t_stack **a, t_stack **b)
+{
+
+}
+
+static void complex_sort(t_stack **a, t_stack **b)
+{
 
     
 }
